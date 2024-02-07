7a0313b9e4617c01a4a1909631d6f4e6c5675c96
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2024-02-07 10:39:30 +0200

    Creating AlternativesEntity

--- src/main/java/com/jmahanuque/certification/modules/questions/entities/AlternativesEntity.java
+++ src/main/java/com/jmahanuque/certification/modules/questions/entities/AlternativesEntity.java
@@ -1,31 +1,30 @@
 package com.jmahanuque.certification.modules.questions.entities;
 
-
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity(name = "questions")
-public class QuestionEntity {
+@Entity
+public class AlternativesEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @Column(length = 50)
-    private String technology;
-
     private String description;
 
-    private List<AlternativesEntity> alternatives;
+    private boolean isCorrect;
 
     @CreationTimestamp
     private LocalDateTime createdAt;
