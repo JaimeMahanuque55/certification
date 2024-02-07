c43c5323c0c6f78fd7516f4394c904d0cede2b77
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2024-02-07 10:27:11 +0200

    Creating CertificationStudentRepository

--- src/main/java/com/jmahanuque/certification/modules/students/useCases/VerifyIfHasCertificationUseCase.java
+++ src/main/java/com/jmahanuque/certification/modules/students/useCases/VerifyIfHasCertificationUseCase.java
@@ -1,13 +1,19 @@
 package com.jmahanuque.certification.modules.students.useCases;
 
 import com.jmahanuque.certification.modules.students.dto.VerifyIfHasCertificationDTO;
+import com.jmahanuque.certification.modules.students.repositories.CertificationStudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VerifyIfHasCertificationUseCase {
 
+    @Autowired
+    private CertificationStudentRepository certificationStudentRepository;
+
     public boolean execute(VerifyIfHasCertificationDTO dto ) {
-        if (dto.getEmail().equals("jaime@gmail.com") && dto.getTechnology().equals("JAVA")) {
+       var result = this.certificationStudentRepository.findByStudentEmailAndTechnology(dto.getEmail(), dto.getTechnology());
+        if (!result.isEmpty()) {
             return true;
         }
         return false;
