e05d8c8f228b48cd9e70b033857e4f8c04aa1ba2
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2024-02-08 19:34:18 +0200

    Get questions alternatives (Correct or incorrect) on StudentCertificationAnswersUseCase

--- src/main/java/com/jmahanuque/certification/modules/students/useCases/StudentCertificationAnswersUseCase.java
+++ src/main/java/com/jmahanuque/certification/modules/students/useCases/StudentCertificationAnswersUseCase.java
@@ -1,9 +1,13 @@
 package com.jmahanuque.certification.modules.students.useCases;
 
+import com.jmahanuque.certification.modules.questions.entities.QuestionEntity;
+import com.jmahanuque.certification.modules.questions.repositories.QuestionRepository;
 import com.jmahanuque.certification.modules.students.dto.StudentCertificationAnswerDTO;
 import com.jmahanuque.certification.modules.students.repositories.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class StudentCertificationAnswersUseCase {
@@ -11,7 +15,10 @@ public class StudentCertificationAnswersUseCase {
     @Autowired
     private StudentRepository studentRepository;
 
-    public void execute(StudentCertificationAnswerDTO dto) throws Exception{
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    public StudentCertificationAnswerDTO execute(StudentCertificationAnswerDTO dto) throws Exception{
         // verify if user exists
         var student = studentRepository.findByEmail(dto.getEmail());
 
@@ -19,10 +26,23 @@ public class StudentCertificationAnswersUseCase {
             throw new Exception("E-mail do estudante incorrecto");
         }
 
-
-
         // Get questions alternatives (Correct or incorrect)
+        List<QuestionEntity> questionsEntity = questionRepository.findByTechnology(dto.getTechnology());
 
+        dto.getQuestionsAnswers().stream().forEach(questionAnswer -> {
+            var question = questionsEntity.stream().filter(q -> q.getId().equals(questionAnswer.getQuestionID()))
+                    .findFirst().get();
+
+            var findCorrectAlternative = question.getAlternatives().stream().filter(alternative -> alternative.isCorrect()).findFirst().get();
+
+            if (findCorrectAlternative.getId().equals(questionAnswer.getAlternativeID())) {
+                questionAnswer.setCorrect(true);
+            } else {
+                questionAnswer.setCorrect(false);
+            }
+        });
+
+        return dto;
         // Save the information of certifications
     }
 }
