6c70f5e55ab17c84ae5cd3742be9aa1123d7359d
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2024-02-08 08:45:05 +0200

    creating QuestionAnswerDTO

---
+++ src/main/java/com/jmahanuque/certification/modules/students/dto/QuestionAnswerDTO.java
@@ -0,0 +1,18 @@
+package com.jmahanuque.certification.modules.students.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class QuestionAnswerDTO {
+
+    private String questionID;
+    private String alternativeID;
+}
