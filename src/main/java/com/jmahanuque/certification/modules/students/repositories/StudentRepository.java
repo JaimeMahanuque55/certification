98832919317ad71da7f562abac332735791d006c
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2024-02-08 08:56:41 +0200

    changing from return StudentEntity to return Option<StudentEntity>

--- src/main/java/com/jmahanuque/certification/modules/students/repositories/StudentRepository.java
+++ src/main/java/com/jmahanuque/certification/modules/students/repositories/StudentRepository.java
@@ -3,9 +3,10 @@ package com.jmahanuque.certification.modules.students.repositories;
 import com.jmahanuque.certification.modules.students.entities.StudentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface StudentRepository extends JpaRepository<StudentEntity, UUID> {
 
-    public StudentEntity findByEmail(String email);
+    public Optional<StudentEntity> findByEmail(String email);
 }
